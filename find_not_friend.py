ef6c60723c23d7af4f218450d393f66951422575
meongju0o0 <bl4angel20@khu.ac.kr>
2023-05-20 00:57:07 +0900

    first commit

---
+++ find_not_friend.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+
+following_url = input("following 주소: ")
+follower_url = input("follower 주소: ")
+
+following_html = requests.get(following_url).text
+follower_html = requests.get(follower_url).text
+
+following_soup = BeautifulSoup(following_html, 'html5lib')
+follower_soup = BeautifulSoup(follower_html, 'html5lib')
+
+following_result = following_soup.select('turbo-frame a span.f4.Link--primary')
+following_list = []
+
+follower_result = follower_soup.select('turbo-frame a span.f4.Link--primary')
+follower_list = []
+
+for following in following_result:
+    following_list.append(following.text)
+
+for follower in follower_result:
+    follower_list.append(follower.text)
+
+following_list = [_ for _ in following_list if _ != '']
+follower_list = [_ for _ in follower_list if _ != '']
+
+following_set = set(following_list)
+follower_set = set(follower_list)
+
+only_following = following_set - follower_set
+only_follower = follower_set - following_set
+
+print("only following: ")
+for following in only_following:
+    print(following, sep=', ')
+
+print("only follower: ")
+for follower in only_follower:
+    print(follower, sep=', ')
+
